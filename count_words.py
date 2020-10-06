aac73304f5a65aa06cdc996f30956f5afa2faffc
lonelyJOJO-s <1207833477@qq.com>
2020-10-06 16:05:51 +0800

    update

--- count_words.py
+++ count_words.py
@@ -1,2 +1,9 @@
-inp = input("Enter a line:")
-print(len(inp.split()))
+from collections import Counter
+import re
+
+# inp = input("Enter a line:")
+# print(len(inp.split()))
+path = 'A good man is hard to fin.txt'
+words = re.findall('\w+', open(path, encoding='UTF-8').read().lower())
+print(Counter(words).most_common(10))
+
